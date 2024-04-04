55c2fa2b74915416ec2a077b4159c0e60726bb2c
ryougi1 <gulong.james@gmail.com>
2024-04-04 17:52:46 +0200

    #feat: mint nft

--- src/pages/nft/[permalink].tsx
+++ src/pages/nft/[permalink].tsx
@@ -1,12 +1,5 @@
-import React from 'react';
-import {
-  ConnectWallet,
-  useActiveClaimCondition,
-  useAddress,
-  useClaimedNFTs,
-  useContract,
-  useUnclaimedNFTs,
-} from '@thirdweb-dev/react';
+import React, { useEffect, useState } from 'react';
+import { ConnectWallet, useAddress, useContract } from '@thirdweb-dev/react';
 import { sanityClient, urlFor } from '../../../sanityClient';
 import { Collection } from '../../../typings';
 import { GetServerSidePropsContext } from 'next';
@@ -18,18 +11,57 @@ interface Props {
 
 export default function NFTDropPage({ collection }: Props) {
   const address = useAddress();
-  // console.log('Address:', address);
   const contract = useContract(collection.smartContractAddress, 'nft-drop').contract;
-  const activeClaimCondition = useActiveClaimCondition(contract).data;
-  // console.log('ActiveClaimCondition', activeClaimCondition);
-  const claimedNFTs = useClaimedNFTs(contract).data;
-  // console.log('Claimed', claimedNFTs);
-  const unclaimedNFTs = useUnclaimedNFTs(contract).data;
-  // console.log('Unclaimed', unclaimedNFTs);
+
+  const [supply, setSupply] = useState<number[] | undefined>(undefined);
+  const [ownedNFTs, setOwnedNFTs] = useState<any[]>();
+  const [mintedNFT, setMintedNFT] = useState<any | undefined>(undefined);
+  const [activeClaimCondition, setActiveClaimCondition] = useState<any | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchOwnedNFTs = async () => {
+      if (contract && address) {
+        const res = await contract.getOwned(address);
+        setOwnedNFTs(res);
+      } else {
+        setOwnedNFTs([]);
+      }
+    };
+    const updateSupply = async () => {
+      if (contract) {
+        const claimedRes = await contract.getAllClaimed();
+        const unClaimedRes = await contract.getAllUnclaimed();
+        setSupply([claimedRes.length, unClaimedRes.length]);
+      }
+    };
+    const fetchClaimCondition = async () => {
+      if (contract) {
+        const res = await contract.claimConditions.getActive();
+        setActiveClaimCondition(res);
+      }
+    };
+    fetchOwnedNFTs();
+    updateSupply();
+    fetchClaimCondition();
+  }, [contract, address, mintedNFT]);
+
+  const mintNFT = async () => {
+    if (!address || !contract) return;
+    contract
+      .claimTo(address, 1)
+      .then(async (tx) => {
+        const receipt = tx[0].receipt;
+        const nft = await tx[0].data();
+
+        console.log('Receipt', receipt);
+        console.log('NFT', nft);
+        setMintedNFT({ ...receipt, ...nft });
+      })
+      .catch((err) => console.log('Transaction error', err));
+  };
 
   return (
-    <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
-      {/* LEFT */}
+    <div className="flex h-screen flex-col lg:grid lg:grid-cols-10 relative">
       <div className="bg-gradient-to-br from-cyan-800 to-rose-500 lg:col-span-4">
         <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
           <div className="rounded-xl bg-gradient-to-br from-[#385783] to-[#EF8432] p-2">
@@ -66,11 +98,12 @@ export default function NFTDropPage({ collection }: Props) {
         </header>
         <hr className="my-4 border" />
         {address && (
-          <p className="text-center text-sm text-green-500">
+          <p className="text-center text-sm font-bold text-gray-500">
             You are logged in with wallet {address.substring(0, 5)}...
             {address.substring(address.length - 5)}
           </p>
         )}
+
         <div className="mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:justify-center lg:space-y-0">
           <img
             src={urlFor(collection.mainImage).url()}
@@ -80,9 +113,9 @@ export default function NFTDropPage({ collection }: Props) {
           <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">
             The {collection.nftCollectionName} NFT Drop
           </h1>
-          {claimedNFTs && unclaimedNFTs ? (
+          {supply ? (
             <p className="pt-2 text-xl text-green-500">
-              {claimedNFTs.length}/{unclaimedNFTs.length} NFTs claimed
+              {supply[0]}/{supply[1]} NFTs claimed
             </p>
           ) : (
             <div className="relative">
@@ -94,22 +127,23 @@ export default function NFTDropPage({ collection }: Props) {
               />
             </div>
           )}
+          {address && ownedNFTs && (
+            <div>
+              <span>You own {ownedNFTs.length} NFTs from this collection</span>
+            </div>
+          )}
         </div>
+
         <button
-          disabled={
-            !claimedNFTs ||
-            !unclaimedNFTs ||
-            !activeClaimCondition ||
-            !address ||
-            claimedNFTs.length === unclaimedNFTs.length
-          }
+          onClick={mintNFT}
+          disabled={!supply || !activeClaimCondition || !address || supply[0] === supply[1]}
           className="mt-10 h-16 w-full rounded-full bg-red-600 text-white font-bold disabled:bg-gray-400"
         >
-          {!claimedNFTs || !unclaimedNFTs || !activeClaimCondition ? (
+          {!supply || !activeClaimCondition ? (
             <>Loading</>
           ) : !address ? (
-            <>Sign in to mint</>
-          ) : claimedNFTs.length === unclaimedNFTs.length ? (
+            <>Login with wallet to mint</>
+          ) : supply[0] === supply[1] ? (
             <>Sold out</>
           ) : (
             <span className="font-bold">
